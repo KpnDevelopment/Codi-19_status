9e5d96061d8648965c278cb178a00884ef844e95
Vishnu V <vishnukpn884@gmail.com>
2021-05-31 09:53:30 +0530

    Updates

--- src/utils/url.js
+++
@@ -1,3 +0,0 @@
-export const totalSummaryUrl = "https://api.covid19api.com/summary";
-export const indiaUrl = "https://api.covid19api.com/live/country/india/status/confirmed";
-export const CountryListUrl = "https://api.covid19api.com/countries";
